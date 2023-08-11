294df621df0837c8db8dc53d6875ea719511fb90
jschwind <info@juergen-schwind.de>
2023-08-11 21:14:11 +0200

    Probleme mit der .htaccess behoben

--- source/php/pages/code_mensch2.inc.php
+++ source/php/pages/code_mensch2.inc.php
@@ -16,6 +16,12 @@ require_once OSWMENSCH_CORE_ABSPATH.'php'.DIRECTORY_SEPARATOR.'header.inc.php';
 
 use \JBSNewMedia\GitInstall\Installer;
 
+$htaccess_content_old='';
+$htaccess=OSWMENSCH_CORE_ABSPATH.'.htaccess2';
+if (file_exists($htaccess)) {
+	$htaccess_content_old=file_get_contents($htaccess);
+}
+
 $Installer=new Installer();
 $Installer->setRealPath(OSWMENSCH_CORE_ABSPATH);
 $Installer->setChmodFile(\osWMensch\Server\Configure::getValueAsInt('chmod_file'));
@@ -31,16 +37,37 @@ $Installer->setLocalPath('');
 $Installer->setAction('execute');
 $result=$Installer->runEngine();
 
+$htaccess=OSWMENSCH_CORE_ABSPATH.'.htaccess';
+if (file_exists($htaccess)) {
+	$htaccess_content_new=file_get_contents($htaccess);
+}
+
+if ($htaccess_content_old!=$htaccess_content_new) {
+	print_a($htaccess_content_old);
+	print_a($htaccess_content_new);
+	$start=strpos($htaccess_content_old, '### mensch2 start ###');
+	$end=strpos($htaccess_content_old, '### mensch2-end ###');
+	print_a($start);
+	print_a($end);
+	$htaccess_content='';
+	if (($end===false)||($start===false)) {
+		$htaccess_content=$htaccess_content_old.$htaccess_content_new;
+	} else {
+		$htaccess_content=substr($htaccess_content_old, 0, $start).$htaccess_content_new.substr($htaccess_content_old, $end+strlen('### mensch2-end ###'));
+	}
+	file_put_contents($htaccess, $htaccess_content);
+}
+
 ?>
 
-<div class="card shadow mb-4">
-	<div class="card-header py-3">
-		<h6 class="m-0 font-weight-bold text-primary">mensch² update</h6>
+	<div class="card shadow mb-4">
+		<div class="card-header py-3">
+			<h6 class="m-0 font-weight-bold text-primary">mensch² update</h6>
+		</div>
+		<div class="card-body">
+			<?php echo $result['result_message'];?>
+		</div>
 	</div>
-	<div class="card-body">
-		<?php echo $result['result_message'];?>
-	</div>
-</div>
 
 <?php
 
